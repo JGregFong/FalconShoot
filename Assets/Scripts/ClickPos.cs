66b2a8c729e9dd1029b0fe7e7bb6c96e2d2e0279
Jonathan Fong <jgregfong@gmail.com>
2019-10-30 16:06:05 -0700

    Added main files.

---
+++ Assets/Scripts/ClickPos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickPos : MonoBehaviour
+{
+    public LayerMask clickMask;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = -Vector3.one;
+            Ray raybeam = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+
+            if (Physics.Raycast(raybeam, out RaycastHit hit))
+            {
+                clickPosition = hit.point;
+            }
+            Debug.Log(clickPosition);
+        }
+
+    }
+}
