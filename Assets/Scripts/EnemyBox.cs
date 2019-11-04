3927ad348ce2d43d62824d86f1af6e6e76239ebf
Jonathan Fong <jgregfong@gmail.com>
2019-11-03 21:08:50 -0800

    Started on a couple scripts. Enemy Box and Turret Fire.

--- Assets/Scripts/EnemyBox.cs
+++ Assets/Scripts/EnemyBox.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class TurretFire : MonoBehaviour
+public class EnemyBox : MonoBehaviour
 {
     // Start is called before the first frame update
-    GameObject Emitter;
     void Start()
     {
-        Emitter = GameObject.Find("TurretOutput");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Blaster Bolt
-
-        }
+        
     }
 }
